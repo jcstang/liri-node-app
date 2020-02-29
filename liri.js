c70c0b48961da9293d6125a79a3e91a6b60b3208
Jacob Stanger <jacobcstanger@gmail.com>
2020-02-28 21:36:38 -0700

    setup a TODO

--- liri.js
+++ liri.js
@@ -6,16 +6,4 @@ let spotify = new Spotify(keys.mySpotifyCredentials);
 
 console.log(spotify);
 
-
-// command line commands:
-// concert-this
-// node liri.js concert-this <artist/band name here>
-
-// spotify-this-song
-//node liri.js spotify-this-song '<song name here>'
-
-// movie-this
-// node liri.js movie-this '<movie name here>'
-
-// do-what-it-says
-// node liri.js do-what-it-says
+// TODO: get command line args
