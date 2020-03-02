9034e1b8097a21d7b9032b7a1c8fee8c954990c4
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-02 10:13:37 -0700

    cleanup by pushing to helper-function file

---
+++ helper-functions.js
@@ -0,0 +1,137 @@
+const dotenv = require("dotenv").config();
+const keys = require("./keys.js");
+const axios = require('axios');
+const Spotify = require('node-spotify-api');
+
+let spotify = new Spotify(keys.mySpotifyCredentials);
+let usageMessage = "Usage: node liri.js <command> <searchTerm>";
+
+
+function whichQuestion(argsArray) {
+  const userCommand = argsArray[0];
+  const searchParam = argsArray[1];
+
+  switch (userCommand) {
+    case 'concert-this':
+      helperFuncs.goGetBandData(searchParam);
+      // goGetBandData(args[1]);
+      break;
+    
+    case 'spotify-this-song':
+      helperFuncs.goGetSpotifyData(searchParam);
+      // goGetSpotifyData(args[1]);
+      break;
+    
+    case 'movie-this':
+      helperFuncs.goGetMovieData(searchParam);
+      // goGetMovieData(args[1]);
+      break;
+    
+    case 'do-what-it-says':
+      console.log('do what it says!');
+      break;
+    
+    default:
+      console.log(usageMessage);
+      break;
+  }
+}
+
+
+
+
+function goGetMovieData(searchTerm) {
+  searchTerm = encodeURIComponent(searchTerm.trim());
+  let queryURL = `http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`;
+    
+    axios.get(queryURL)
+    .then(function (response) {
+      // handle success
+      // console.log(response);
+      let movieData = response.data;
+
+      if (movieData.Response === 'False') {
+        return console.log(`No go!... ${movieData.Error}`);
+      } else {
+        // found a movie
+        console.log("-----------------------------------------------");
+        console.log(`Title:\t\t\t ${movieData.Title}`);
+        console.log(`Year:\t\t\t ${movieData.Year}`);
+        console.log(`IMDB rating:\t\t ${movieData.Ratings[0].Value}`);
+        console.log(`Rotten Tomatoes rating:\t ${movieData.Ratings[1].Value}`);
+        console.log(`Movie Produced in:\t ${movieData.Country}`);
+        console.log(`Language:\t\t ${movieData.Language}`);
+        console.log(`Plot:\t ${movieData.Plot}`);
+        console.log(`Actors:\t ${movieData.Actors}`);
+        console.log("-----------------------------------------------");
+
+      }
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+      console.log(
+        `\nBye!`
+      );
+      
+    });
+
+}
+
+function goGetSpotifyData(searchTerm) {
+
+  spotify.search({ type: 'track', query: searchTerm }, function (err, data) {
+    if (err) {
+      return console.log('Error occured: ' + err);
+    }
+
+    for (const obj of data.tracks.items) {
+      // console.log(obj);
+
+      console.log("-----------------------------------------------");
+      console.log(`Artist:\t\t ${obj.album.artists[0].name}`);
+      console.log(`Song:\t\t ${obj.name}`);
+      console.log(`album:\t\t ${obj.album.name}`);
+      console.log(`spotify link:\t ${obj.external_urls.spotify}`);
+      console.log("-----------------------------------------------\n");
+
+      // only getting the first result
+      return;
+    }
+
+  });
+}
+
+function goGetBandData(searchTerm) {
+  // adds the %20 thing in the spaces
+  searchTerm = encodeURIComponent(searchTerm.trim());
+
+  let queryURL = `https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`;
+    
+  axios.get(queryURL)
+    .then(response => {
+      const data0 = response.data[0];
+      console.log(`Artist:\t\t\t ${data0.artist.name}`);
+      console.log(`Name of Venue:\t\t ${data0.venue.name}`);
+      console.log(`Venue location:\t\t ${data0.venue.city}, ${data0.venue.region} ${data0.venue.country}`);
+      console.log(`Date of the Event:\t ${data0.datetime}`);
+
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  .finally(function() {
+    // always executed
+  })
+
+}
+
+module.exports = {
+  goGetMovieData: goGetMovieData,
+  goGetSpotifyData: goGetSpotifyData,
+  goGetBandData: goGetBandData,
+  whichQuestion: whichQuestion
+}
--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
-const dotenv = require("dotenv").config();
-const Spotify = require('node-spotify-api');
-const keys = require("./keys.js");
-const axios = require('axios');
+// const dotenv = require("dotenv").config();
+// const Spotify = require('node-spotify-api');
+// const keys = require("./keys.js");
+const helperFuncs = require('./helper-functions');
 
-let spotify = new Spotify(keys.mySpotifyCredentials);
+// let spotify = new Spotify(keys.mySpotifyCredentials);
 
 let args = process.argv.slice(2);
-let usageMessage = "Usage: node liri.js <command> <searchTerm>";
+// let usageMessage = "Usage: node liri.js <command> <searchTerm>";
 
 
 if (args.length <= 0) {
@@ -16,27 +16,32 @@ if (args.length <= 0) {
 
 console.log('Wait for it......\n');
 
-switch (args[0]) {
-  case 'concert-this':
-    goGetBandData(args[1]);
-    break;
+helperFuncs.whichQuestion(args);
 
-  case 'spotify-this-song':
-    goGetSpotifyData(args[1]);
-    break;
+// switch (args[0]) {
+//   case 'concert-this':
+//     helperFuncs.goGetBandData(args[1]);
+//     // goGetBandData(args[1]);
+//     break;
 
-  case 'movie-this':
-    goGetMovieData(args[1]);
-    break;
+//   case 'spotify-this-song':
+//     helperFuncs.goGetSpotifyData(args[1]);
+//     // goGetSpotifyData(args[1]);
+//     break;
 
-  case 'do-what-it-says':
-    console.log('do what it says!');
-    break;
+//   case 'movie-this':
+//     helperFuncs.goGetMovieData(args[1]);
+//     // goGetMovieData(args[1]);
+//     break;
 
-  default:
-    console.log(usageMessage);
-    break;
-}
+//   case 'do-what-it-says':
+//     console.log('do what it says!');
+//     break;
+
+//   default:
+//     console.log(usageMessage);
+//     break;
+// }
 
 
 
@@ -44,96 +49,93 @@ switch (args[0]) {
 // functions
 // ===================================================
 
-function goGetMovieData(searchTerm) {
-  searchTerm = encodeURIComponent(searchTerm.trim());
-  let queryURL = `http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`;
+// function goGetMovieData(searchTerm) {
+//   searchTerm = encodeURIComponent(searchTerm.trim());
+//   let queryURL = `http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`;
     
-    axios.get(queryURL)
-    .then(function (response) {
-      // handle success
-      // console.log(response);
-      let movieData = response.data;
+//     axios.get(queryURL)
+//     .then(function (response) {
+//       // handle success
+//       // console.log(response);
+//       let movieData = response.data;
 
-      if (movieData.Response === 'False') {
-        return console.log(`No go!... ${movieData.Error}`);
-      } else {
-        // found a movie
-        console.log("-----------------------------------------------");
-        console.log(`Title:\t\t\t ${movieData.Title}`);
-        console.log(`Year:\t\t\t ${movieData.Year}`);
-        console.log(`IMDB rating:\t\t ${movieData.Ratings[0].Value}`);
-        console.log(`Rotten Tomatoes rating:\t ${movieData.Ratings[1].Value}`);
-        console.log(`Movie Produced in:\t ${movieData.Country}`);
-        console.log(`Language:\t\t ${movieData.Language}`);
-        console.log(`Plot:\t ${movieData.Plot}`);
-        console.log(`Actors:\t ${movieData.Actors}`);
-        console.log("-----------------------------------------------");
+//       if (movieData.Response === 'False') {
+//         return console.log(`No go!... ${movieData.Error}`);
+//       } else {
+//         // found a movie
+//         console.log("-----------------------------------------------");
+//         console.log(`Title:\t\t\t ${movieData.Title}`);
+//         console.log(`Year:\t\t\t ${movieData.Year}`);
+//         console.log(`IMDB rating:\t\t ${movieData.Ratings[0].Value}`);
+//         console.log(`Rotten Tomatoes rating:\t ${movieData.Ratings[1].Value}`);
+//         console.log(`Movie Produced in:\t ${movieData.Country}`);
+//         console.log(`Language:\t\t ${movieData.Language}`);
+//         console.log(`Plot:\t ${movieData.Plot}`);
+//         console.log(`Actors:\t ${movieData.Actors}`);
+//         console.log("-----------------------------------------------");
 
-      }
+//       }
 
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-      console.log(
-        `\nBye!`
-      );
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+//     .finally(function () {
+//       // always executed
+//       console.log(
+//         `\nBye!`
+//       );
       
-    });
+//     });
 
-  // print this:
-  // title of movie
-  // year movie came out
-  // IMDB rating
-  // rotten tomatoes movie
-  // country where movie was produced
-  // language of movie
-  // plot of the movie (prob short).
-  // Actors in the movie
-}
+// }
 
 
-function goGetSpotifyData(searchTerm) {
+// function goGetSpotifyData(searchTerm) {
 
-  spotify.search({ type: 'track', query: searchTerm }, function (err, data) {
-    if (err) {
-      return console.log('Error occured: ' + err);
-    }
+//   spotify.search({ type: 'track', query: searchTerm }, function (err, data) {
+//     if (err) {
+//       return console.log('Error occured: ' + err);
+//     }
 
-    for (const obj of data.tracks.items) {
-      // console.log(obj);
+//     for (const obj of data.tracks.items) {
+//       // console.log(obj);
 
-      console.log("-----------------------------------------------");
-      console.log(`Artist:\t\t ${obj.album.artists[0].name}`);
-      console.log(`Song:\t\t ${obj.name}`);
-      console.log(`album:\t\t ${obj.album.name}`);
-      console.log(`spotify link:\t ${obj.external_urls.spotify}`);
-      console.log("-----------------------------------------------\n");
+//       console.log("-----------------------------------------------");
+//       console.log(`Artist:\t\t ${obj.album.artists[0].name}`);
+//       console.log(`Song:\t\t ${obj.name}`);
+//       console.log(`album:\t\t ${obj.album.name}`);
+//       console.log(`spotify link:\t ${obj.external_urls.spotify}`);
+//       console.log("-----------------------------------------------\n");
 
-      // only getting the first result
-      return;
-    }
+//       // only getting the first result
+//       return;
+//     }
 
-  });
-}
+//   });
+// }
 
 
-function goGetBandData(searchTerm) {
-  // adds the %20 thing in the spaces
-  searchTerm = encodeURIComponent(searchTerm.trim());
+// function goGetBandData(searchTerm) {
+//   // adds the %20 thing in the spaces
+//   searchTerm = encodeURIComponent(searchTerm.trim());
 
-  let queryURL = `https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`;
+//   let queryURL = `https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`;
+    
+//   axios.get(queryURL)
+//     .then(response => {
+//       const data0 = response.data[0];
+//       console.log(`Artist:\t\t\t ${data0.artist.name}`);
+//       console.log(`Name of Venue:\t\t ${data0.venue.name}`);
+//       console.log(`Venue location:\t\t ${data0.venue.city}, ${data0.venue.region} ${data0.venue.country}`);
+//       console.log(`Date of the Event:\t ${data0.datetime}`);
 
-  axios.get(queryURL)
-    .then(response => {
-      const data0 = response.data[0];
-      console.log(`Artist:\t\t\t ${data0.artist.name}`);
-      console.log(`Name of Venue:\t\t ${data0.venue.name}`);
-      console.log(`Venue location:\t\t ${data0.venue.city}, ${data0.venue.region} ${data0.venue.country}`);
-      console.log(`Date of the Event:\t ${data0.datetime}`);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(function() {
+//     // always executed
+//   })
 
-    });
-
-}
+// }
